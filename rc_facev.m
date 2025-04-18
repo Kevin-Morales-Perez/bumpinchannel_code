5b80ff5ae065b4f765e93410a4e045d2e4d10487
Kevin-Morales-Perez <kevin27182mora@gmail.com>
2025-04-18 01:05:03 -0600

    fuction corrected ,driving pressure term was incorrect and was missing the distance between nodes

--- rc_facev.m
+++ rc_facev.m
@@ -1,7 +1,8 @@
-function [uf] = rc_facev(pP,pA,dp,da,wlsq_op_P,wlsq_op_A,eps,normf_v,uP,uA)
+function [uf] = rc_facev(pP,pA,dp,da,wlsq_op_P,wlsq_op_A,d_eps,eps_vec,normf_v,uP,uA)
 %Rie - Chow interpolation face velocities
 % This function calculates rie-chow face velocities (both components)
 %using the nodal velocities and the gradient pressures
+% inputs
 %pP nodal values of pressures surrounding node P [pPW;pPN;pPE;pPS,pP];
 %pA nodal values of pressures surrounding node A [pAW;pAN;pAE;pAS,pA];
 %dp,da convection coeffitients at nodes P and A (Scalars)
@@ -10,14 +11,17 @@ function [uf] = rc_facev(pP,pA,dp,da,wlsq_op_P,wlsq_op_A,eps,normf_v,uP,uA)
 %normf_v norm unitary vector to face [1,2]
 %eps unitary vector in the P_A direction [2,1]
 %uP,uA nodal velocities at points P and A [u,v]
+%d_eps distance between nodes (scalar)
 
-%________________________computing pressure gradients
+%Output
+%uf(Scalar)
+%________________________computing pressure gradients______________________
 pnodP=pP(5);
 pnodA=pA(5);
 delt_pP=pP(1:4)-pnodP;
 delt_pA=pA(1:4)-pnodA;
 gradpP=wlsq_op_P*delt_pP;
 gradpA=wlsq_op_A*delt_pA;
-%________________________main formula 
-uf= 0.5*dot((uP+uA),normf_v) +0.5*(dp+da)*(pnodP + pnodA)/eps -0.5*dot(((dp*gradpP) + (da*gradpA)),eps);
+%________________________main formula _____________________________________
+uf= 0.5*dot((uP+uA),normf_v) +0.5*(dp+da)*(pnodP - pnodA)/d_eps -0.5*dot(((dp*gradpP) + (da*gradpA)),eps_vec);
 end
