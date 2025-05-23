3287b7352edd0a8af545dfc578af10d05f3c01d1
Kevin-Morales-Perez <kevin27182mora@gmail.com>
2025-05-23 04:51:52 -0600

    Enhaced pressure boundary conditions function

--- p_press_boundaries.m
+++ p_press_boundaries.m
@@ -1,4 +1,5 @@
-function [p_press] = p_press_boundaries(p_press,n_x,n_y)
+function [p_press] = p_press_boundaries(p_press,n_x,n_y,y_ghost_cells,...
+    x_wall_cells,y_wall_cells)
 %Boundary conditions for pressure
 %Bump in channel problem 
 
@@ -16,18 +17,45 @@ function [p_press] = p_press_boundaries(p_press,n_x,n_y)
 
     %######### OUTLET ###########
     %#######  OUTFLOW  ############
-    %p_press(:,n_x-1)=p_press(:,n_x-2);% To be reviewed 
-
-    %############   ON BUMP  ##################
-    %## Neumman(Normal to the wall) ###########
-    p_press(n_y-1, le_index:te_index) = p_press(n_y-2, le_index:te_index);%to be reviewed but try
+    p_press(:,n_x-1)=1.02828*p_press(:,n_x-2);% To be reviewed 
 
     %#### OUTSIDE THE BUMP ####### 
     %####### Neumman ###########
-    %inlet flow %to be reviewed
-    p_press(n_y-1,1:le_index)=p_press(n_y-2,1:le_index);
-    %outlow flow%to be reviewed
-    p_press(n_y-1,te_index:n_x-1)=p_press(n_y-2,te_index:n_x-1);
+    %inlet and outlow flow 
+    p_press(n_y-1,:)=p_press(n_y-2,:);
+ 
 
+    %############   ON BUMP  ##################
+    %## Neumman(Normal to the wall) ###########
+    %NEW CODE TO ENHACE NEUMMAN BOUNDARY CONDITION  ON BUMP
+    %GET NODE COORDINATES OF CELLS THAT ARE ON THE WALL
+    %GET NODE COORDINATES OF BOTTOM GHOST CELLS (BENEATH THE WALL)
+    for i=le_index:te_index
+        %determine sign of  SLOPE in ghost cells
+        slope=y_ghost_cells(i+1)-y_ghost_cells(i);
+        if slope >0
+            theta_p=atan(slope);
+            delta_y=y_wall_cells(i+1)-y_ghost_cells(i+1);
+            dist_a=delta_y*sin(theta_p);
+            distance_p=sqrt((x_wall_cells(i+1)-x_wall_cells(i))^2+...
+                (y_wall_cells(i+1)-y_wall_cells(i))^2);
+            diff_press=p_press(n_y-2,i+1)-p_press(n_y-2,i);
+            p_inter=p_press(n_y-2,i) + (diff_press/distance_p)*(distance_p-...
+                dist_a);
+            p_press(n_y-1,i+1)=p_inter;
+       
+        elseif slope<0
+            theta_p=atan(slope);
+            delta_y=y_wall_cells(i)-y_ghost_cells(i);
+            dist_a=delta_y*sin(theta_p);
+            distance_p=sqrt((x_wall_cells(i+1)-x_wall_cells(i))^2+...
+                (y_wall_cells(i+1)-y_wall_cells(i))^2);
+            diff_press=p_press(n_y-2,i+1)-p_press(n_y-2,i);
+            p_inter=p_press(n_y-2,i) + (diff_press/distance_p)*dist_a;
+            p_press(n_y-1,i+1)=p_inter;
+        else
+            p_press(n_y-1,i+1)=p_press(n_y-2,i+1);
+        end
+    end
 end
 
